65551e14cfdc83d31951731df67c881a1896298b
ishida530 <63319022+ishida530@users.noreply.github.com>
2020-10-13 09:40:36 +0200

    Add files via upload
    HTML/SASS/BOOTSTRAP

---
+++ js.js
@@ -0,0 +1,55 @@
+var bar = new RadialProgress(document.getElementById("skill1"), {
+    progress: -6,
+    colorBg: "white",
+    colorFg: "#F5317F",
+    colorText: "black",
+    thick: 6,
+    round: "true"
+
+});
+bar.setIndeterminate(false);
+bar.setValue(.75);
+var bar = new RadialProgress(document.getElementById("skill2"), {
+    progress: -6,
+    colorBg: "white",
+    colorFg: "#B122E5",
+    colorText: "black",
+    thick: 6,
+    round: "true"
+
+});
+bar.setIndeterminate(false);
+bar.setValue(.7);
+var bar = new RadialProgress(document.getElementById("skill3"), {
+    progress: -6,
+    colorBg: "white",
+    colorFg: "#1270E3",
+    colorText: "black",
+    thick: 6,
+    round: "true"
+
+});
+bar.setIndeterminate(false);
+bar.setValue(.95);
+var bar = new RadialProgress(document.getElementById("skill4"), {
+    progress: -6,
+    colorBg: "white",
+    colorFg: "#2CBFC7",
+    colorText: "black",
+    thick: 6,
+    round: "true"
+
+});
+bar.setIndeterminate(false);
+bar.setValue(.6);
+var bar = new RadialProgress(document.getElementById("skill5"), {
+    progress: -6,
+    colorBg: "white",
+    colorFg: "#F5317F",
+    colorText: "black",
+    thick: 6,
+    round: "true"
+
+});
+bar.setIndeterminate(false);
+bar.setValue(.6);
